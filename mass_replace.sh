d7afa71c9a3d64c179b5a6e391b0a4c6a770ce4d
Tomas Overdrive Petru <over@labka.cz>
2016-11-06 20:48:49 +0100

    replace divnejch znaku, replace charsetu

--- mass_replace.sh
+++ mass_replace.sh
@@ -6,4 +6,6 @@
 
 #brew install gnu-sed
 gsed -i -- 's/http:\/\/overdrive.a-nihil.net\//https:\/\/over23.github.io\//g' *.html
-
+gsed -i -- 's/ľ/ž/g' *.html
+gsed -i -- 's/ą/š/g' *.html
+gsed -i -- 's/charset=ISO-8859-2/UTF-8/g' *.html
